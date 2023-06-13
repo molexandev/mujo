2201395d50e90c46f683c6abadbd5c0a20b18ce2
molexandev01~ <molexandev@gmail.com>
2023-06-13 18:17:24 +0300

    added product page

--- app/js/product.js
+++ app/js/product.js
@@ -1,23 +1,27 @@
 $(function () {
+    // SLIDER
 
-   // SLIDER
+    $(".slider-product__big").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: ".slider-product__thumb",
+    });
 
-   $(".slider-product__big").slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      fade: true,
-      asNavFor: '.slider-product__thumb'
-   });
+    $(".slider-product__thumb").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        asNavFor: ".slider-product__big",
+    });
 
-   $(".slider-product__thumb").slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      dots: true,
-      centerMode: true,
-      focusOnSelect: true,
-      asNavFor: '.slider-product__big' 
-  });
+    // SlideToggle
 
-
+    $(".faq-content__title").on("click", function () {
+        $(this).toggleClass("faq-content__title--active");
+        $(this).next().slideToggle();
+    });
 });
